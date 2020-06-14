b8657ec28118aa6732940bcaa1f282e4b4199b2b
Mustafa Cagri Peker <mustafacagripeker@outlook.com>
2020-06-15 01:41:27 +0300

    Initial Commit

---
+++ AlSatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlSatApp
+//
+//  Created by Mustafa Çağrı Peker on 15.06.2020.
+//  Copyright © 2020 Mustafa Cagri Peker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
